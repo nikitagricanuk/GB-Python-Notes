18d2dbc2cc105e56c1942930e3ae5507778f7ede
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-09-23 16:10:59 +0800

    Added note_id

--- args.py
+++ args.py
@@ -4,6 +4,8 @@ def parse_args(args):
             note_title = args[i+1]
         elif args[i] == "--msg":
             note_body = args[i+1]
+        elif args[i] == "--id":
+            note_id = args[i+1]   
         else:
             print(f"Unknown option: {args[i]}")
-    return note_title, note_body
+    return note_id, note_title, note_body
